f6e3687525887f050971348a6495960e3c6419d1
AlexanderMcArovHP <alexandercom445@gmail.com>
2020-10-05 16:38:09 +0600

    Update Header

--- src/components/Store/Store.js
+++ src/components/Store/Store.js
@@ -5,7 +5,7 @@ function Store(props) {
 
     return (
         <div className={Style.Store}>
-            Store
+            <img src="https://pix10.agoda.net/hotelImages/973842/-1/f47b4dad670816c6862278708f30ffeb.jpg?s=1024x768"></img>
         </div>
     )
 }
